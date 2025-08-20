e4ba6cf9cbfef25c4e10e9df028f16fce6223a51
Rozdorozhnii <rozdorozhnii@ukr.net>
2025-08-20 15:58:41 +0300

    auth service app added & dockerized

--- api/apps/auth/src/main.ts
+++ api/apps/auth/src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 
-import { NotesModule } from './notes.module';
+import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotesModule);
+  const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger)); // Optional: Set up a logger if needed
-  await app.listen(process.env.port ?? 3001);
-}
 
-void bootstrap();
+  await app.listen(process.env.port ?? 3002);
+}
+bootstrap();
